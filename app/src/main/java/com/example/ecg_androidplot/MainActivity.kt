d79945bd566ea889c1ab64b5be8b1d0a86f475d4
Antoine <antoine.chalon@gmail.com>
2020-03-19 19:31:40 +0100

    mediaplayer realease On Stop added

--- app/src/main/java/com/example/ecg_androidplot/MainActivity.kt
+++ app/src/main/java/com/example/ecg_androidplot/MainActivity.kt
@@ -168,6 +168,6 @@ class MainActivity : Activity() {
     public override fun onStop() {
         super.onStop()
         redrawer!!.finish()
-        mediaPlayer?.release()
+        mediaPlayer?.release()//
     }
 }
